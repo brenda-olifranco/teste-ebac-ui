ada9968bc33d6a34a2e7e7687a0fe08b2026483d
Brenda Franco <brenda_olifranco@hotmail.com>
2025-01-02 15:57:58 -0300

    testando lista de produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,17 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            .eq(3)
+            //.contains('Ajax Full-Zip Sweatshirt')
+            .click()
+
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+    });
+});
