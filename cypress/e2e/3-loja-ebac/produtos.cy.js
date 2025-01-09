b4f06841bd53116f9b515bd25488fa60e06e80e9
Brenda Franco <brenda_olifranco@hotmail.com>
2025-01-09 20:43:41 -0300

    mudança no origin main

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,17 +1,52 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.product-block')
-            .eq(3)
-            //.contains('Ajax Full-Zip Sweatshirt')
-            .click()
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
 
             cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+
+    it('Deve buscar um produto com sucesso', () => {
+        produtosPage.buscarProduto('Aero Daily Fitness Tee')
+
+        cy.get('.product_title').should('exist')
+    });
+
+    it('Deve visitar a página do produto ', () => {
+        produtosPage.visitarProduto('apollo running short')
+        cy.get('.product_title').should('exist')
+
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 5
+        produtosPage.buscarProduto('Ariel Roll Sleeve Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Green', qtd )
+        cy.get('.woocommerce-message').should('exist')
+
+    });
+
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho, 
+            dados[0].cor, 
+            dados[0].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+        })
+
+        
+
+    });
 });
+
+    
